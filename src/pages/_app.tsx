d5838206213b5e570f619307d99b3ba59b2aefab
Mayur Dayal <mayur.dayal97@gmail.com>
2024-09-29 14:57:57 +0530

    feat(ui): add form components and toast notifications

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import { Toaster } from "@/components/ui/toaster";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
@@ -9,6 +10,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <title>Mayur Dayal | Frontend-focused SDE Portfolio</title>
       </Head>
       <Component {...pageProps} />
+      <Toaster />
     </>
   );
 }
